82df1cfc10be9d9a5a45a19e6ca94c34908e0f2c
PedroGomes4156 <phsgoficial78@gmail.com>
2026-08-08 12:44:31 +0000

    apredendi a usar o componente text

---
+++ aula-08-08-text.jsx
@@ -0,0 +1,21 @@
+import { Text, View } from 'react-native';
+
+
+export default function App() {
+  return (
+    <View style={{ flex:1, backgroundColor: 'blue'}}>
+
+    <View style={{ flex:11, backgroundColor: 'red'}} >
+    <Text>UMaplicativoSinistro</Text>
+    </View>
+
+    <View style={{ flex:10, backgroundColor: 'gray'}} />
+
+     <View style={{ flex:64, backgroundColor: 'green'}} />
+
+     <View style={{ flex:8, backgroundColor: 'yellow'}} />
+
+    </View>
+    
+  );
+}
