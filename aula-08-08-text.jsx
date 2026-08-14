24433b511d6356dc94968bed8a91b28a9881b434
PedroGomes4156 <phsgoficial78@gmail.com>
2026-08-14 07:58:43 -0300

    Update aula-08-08-text.jsx

--- aula-08-08-text.jsx
+++ aula-08-08-text.jsx
@@ -1,17 +1,24 @@
-import { Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
 
 
 export default function App() {
   return (
     <View style={{ flex:1, backgroundColor: 'blue'}}>
 
-    <View style={{ flex:11, backgroundColor: 'red'}} >
-    <Text>UMaplicativoSinistro</Text>
+    <View style={{ flex:11, backgroundColor:'red',alignItems: 'center',justifyContent: 'center'}}>
+
+    <Text style={{fontSize: 32,fontWeight: 'bold', marginTop: 10,}}>UM aplicativo Sinistro</Text>
     </View>
 
-    <View style={{ flex:10, backgroundColor: 'gray'}} />
+    <View style={{ flex:10, backgroundColor: 'gray'}}/>
 
-     <View style={{ flex:64, backgroundColor: 'green'}} />
+     <View style={{ flex:64, backgroundColor: 'green'}}>
+     <Image source={{uri:''}}
+     style={{
+       width:500,
+       height:560
+     }}/>
+     </View>
 
      <View style={{ flex:8, backgroundColor: 'yellow'}} />
 
